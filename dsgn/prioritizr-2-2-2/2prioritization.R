6b750517f04099d02e7e0de9f81fc6b8ca8685ba
Carina-Firkowski <carina.firkowski@apexrms.com>
2025-07-01 16:15:07 -0400

    Move version 2.2.2 to dsgn

---
+++ dsgn/prioritizr-2-2-2/2prioritization.R
@@ -0,0 +1,98 @@
+## prioritizr SyncroSim - Prioritization
+##
+## Written by Carina Rauen Firkowski
+##
+## This script loads a prioritizr problem formulation and solves it. It requires
+## that the model stage "Problem formulation" be run first.
+
+
+
+# Solve and save solution ------------------------------------------------------
+
+progressBar(type = "message", message = "Solving problem")
+
+# Run solver
+scenarioSolution <- solve(scenarioProblem)
+
+# Save raster
+if(class(scenarioSolution) != "data.frame"){
+  solutionFilename <- file.path(paste0(dataDir, "\\solutionRaster.tif"))
+  writeRaster(scenarioSolution, filename = solutionFilename,
+              overwrite = TRUE)
+  # Save file path to datasheet
+  solutionRasterOutput <- data.frame(solution = solutionFilename)
+  saveDatasheet(ssimObject = myScenario, 
+                data = solutionRasterOutput, 
+                name = "prioritizr_solutionRasterOutput")
+}
+# Save tabular output
+if(class(scenarioSolution) == "data.frame"){
+  
+  # Open datasheet
+  solutionTabularOutput <- datasheet(myScenario,
+                                     name = "prioritizr_solutionTabularOutput")
+  
+  # Rename variable names for consistency with prioritizr R
+  names(solutionTabularOutput)[6] <- "solution_1"
+  
+  # Combine result and datasheet
+  solutionTabularOutput <- merge(solutionTabularOutput, 
+                                 scenarioSolution, 
+                                 all = TRUE)
+  # Reorder columns
+  solutionTabularOutput <- data.frame(id = solutionTabularOutput$id,
+                                      cost = solutionTabularOutput$cost,
+                                      status = solutionTabularOutput$status,
+                                      xloc = solutionTabularOutput$xloc,
+                                      yloc = solutionTabularOutput$yloc,
+                                      solution_1 = solutionTabularOutput$solution_1)
+  # Remove all NA rows
+  solutionTabularOutput <- solutionTabularOutput[
+   rowSums(is.na(solutionTabularOutput)) != ncol(solutionTabularOutput), ]
+  
+  # Rename column
+  names(solutionTabularOutput)[6] <- "solution1"
+  
+  # Save solution tabular results
+  saveDatasheet(ssimObject = myScenario, 
+                data = solutionTabularOutput, 
+                name = "prioritizr_solutionTabularOutput")
+  
+  # Spatial data for visualization
+  if(dim(problemSpatialDatasheet)[1] != 0){
+    if(!is.na(problemSpatialDatasheet$x)){
+      pu_vis <- rast(file.path(problemSpatialDatasheet$x))
+      crs(pu_vis) <- NA
+      puVis <- TRUE
+    }
+  } else {
+    puVis <- FALSE
+  }
+  
+  # Save solution spatial visualization
+  if(isTRUE(puVis)){
+    
+    # Reclass table between planning unit id & solution
+    reclassTable <- matrix(c(scenarioSolution$id,
+                             scenarioSolution$solution_1),
+                           byrow = FALSE, ncol = 2)
+    # Reclassify raster
+    solutionVis <- classify(pu_vis, reclassTable)
+    
+    # Define file path
+    solutionFilepath <- paste0(dataPath, "\\prioritizr_solutionRasterOutput")
+    solutionFilename <- file.path(paste0(solutionFilepath,
+                                         "\\solutionRaster.tif"))
+    # Create directory if it does not exist
+    ifelse(!dir.exists(file.path(solutionFilepath)), 
+           dir.create(file.path(solutionFilepath)), FALSE)
+    writeRaster(solutionVis, filename = solutionFilename, overwrite = TRUE)
+    # Save file path to datasheet
+    solutionRasterOutput <- data.frame(solution = solutionFilename)
+    saveDatasheet(ssimObject = myScenario, 
+                  data = solutionRasterOutput, 
+                  name = "prioritizr_solutionRasterOutput")
+  }
+}
+
+
---
+++ dsgn/prioritizr-2-2-2/basePrioritization.R
@@ -0,0 +1,126 @@
+## prioritizr SyncroSim - Base prioritization transformer
+##
+## Written by Carina Rauen Firkowski
+##
+## This script runs all the steps in a prioritization problem, from problem
+## formulation to solving and evaluating the solution.
+
+
+
+# Workspace --------------------------------------------------------------------
+
+# Load rsyncrosim packages
+library(rsyncrosim) ; progressBar(type = "message", 
+                                  message = "Validating environment and installing missing dependencies")
+
+# Load environment, library, project & scenario
+e <- ssimEnvironment()
+myLibrary <- ssimLibrary()
+myProject <- rsyncrosim::project()
+myScenario <- scenario()
+
+# Open Conda configuration options
+condaDatasheet <- datasheet(myLibrary, name = "core_Option")
+
+# Install missing packages when using Conda
+if(isTRUE(condaDatasheet$UseConda)){
+  
+  # Force binary install of packages
+  options(install.packages.compile.from.source = "never")
+
+  # Validate Rtools installation
+  pathRtools <- Sys.which("make")
+  if(pathRtools != "C:\\rtools40\\usr\\bin\\make.exe"){
+    stop("The installation of Rtools v4.0 is faulty. Please see https://apexrms.github.io/prioritizr/getting_started#troubleshooting-rtools-installation for help.")
+  }
+    
+
+  # Update packahes
+  update.packages(repos='http://cran.us.r-project.org',
+                    ask = FALSE, oldPkgs = "terra", type = "binary")
+  update.packages(repos='http://cran.us.r-project.org', 
+                    ask = FALSE, oldPkgs = "sf", type = "binary")
+
+  # Check which packages to install
+  packagesToCheck <- c("sf","prioritizr", "symphony", "Rsymphony")
+  packagesToInstallList <- packagesToCheck[!(packagesToCheck %in% 
+                                               installed.packages()[,"Package"])]
+  
+  # Install missing packages
+  for(packagesToInstall in packagesToInstallList){
+    if(packagesToInstall == "sf"){
+      install.packages("sf", repos="http://cran.us.r-project.org", type = "binary")
+    }
+    if(packagesToInstall == "prioritizr"){
+      install.packages("https://cran.r-project.org/src/contrib/Archive/prioritizr/prioritizr_8.0.4.tar.gz", 
+                       repos = NULL, type = "source")
+    }
+    if(packagesToInstall == "symphony"){
+      install.packages("https://cran.r-project.org/src/contrib/symphony_0.1.1.tar.gz", 
+                       repos = NULL, type = "source")
+    }
+    if(packagesToInstall == "Rsymphony"){
+      install.packages("Rsymphony", repos="http://cran.us.r-project.org")
+    }
+  }
+}
+
+# Load packages
+progressBar(type = "message", message = "Setting up workspace")
+library(stringr)
+library(terra)
+library(tidyr)
+library(dplyr)
+library(prioritizr)
+library(Rsymphony)
+
+# Data directory
+dataDir <- e$DataDirectory
+
+# Create directory if it does not exist
+ifelse(!dir.exists(file.path(dataDir)), 
+       dir.create(file.path(dataDir)), FALSE)
+
+# Scenario path
+dataPath <- paste(dataDir, paste0("Scenario-", scenarioId(myScenario)), sep="\\") 
+
+
+
+# Model steps ------------------------------------------------------------------
+
+# Problem formulation
+source(file.path(e$PackageDirectory, "1problemFormulation.R"))
+
+# Prioritization
+source(file.path(e$PackageDirectory, "2prioritization.R"))
+
+# Evaluate performance
+source(file.path(e$PackageDirectory, "3evaluatePerformance.R"))
+
+# Evaluate importance
+source(file.path(e$PackageDirectory, "4evaluateImportance.R"))
+
+
+
+# Save intermediate files ------------------------------------------------------
+
+# Problem
+# Write file
+problemFilename <- file.path(paste0(dataPath, "\\problemFormulation.rds"))
+saveRDS(scenarioProblem, file = problemFilename)
+# Save file path to datasheet
+problemOutput <- data.frame(problem = problemFilename)
+saveDatasheet(ssimObject = myScenario, 
+              data = problemOutput,
+              name = "prioritizr_problemFormulation")
+
+# Solution
+# Write file
+solutionObjectFilename <- file.path(paste0(dataPath, "\\solution.rds"))
+saveRDS(scenarioSolution, file = solutionObjectFilename)
+# Save file path to datasheet
+solutionOutput <- data.frame(solution = solutionObjectFilename)
+saveDatasheet(ssimObject = myScenario, 
+              data = solutionOutput, 
+              name = "prioritizr_solutionObject")
+
